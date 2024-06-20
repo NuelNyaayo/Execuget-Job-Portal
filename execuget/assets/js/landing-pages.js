6b7e692563a769c74f6e92eec006d1137a44cd3d
NuelNyaayo <manuelnyaayo@gmail.com>
2024-06-20 23:20:27 +0300

    updated front-pages

---
+++ execuget/assets/js/landing-pages.js
@@ -0,0 +1,8 @@
+//search button from home page
+document.addEventListener('DOMContentLoaded', function() {
+    // Event listener for search button
+    document.getElementById('btnSearch').addEventListener('click', function() {
+        window.location.href = 'popular-categories.html';
+    });
+
+});
